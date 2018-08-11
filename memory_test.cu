a4816bf3649995990e0de0c1cf0df736966fa24d
chennakeshava1998 <16co108.keshava@nitk.edu.in>
2018-08-12 04:13:34 +0530

    temp

--- memory_test.cu
+++ memory_test.cu
@@ -13,6 +13,10 @@
 int cnt;
 void have_fun()
 {
+    // to prevent a seg-fault
+    if(cnt < 130080)
+    return;
+
     printf("Number of function calls: %d\n", cnt);
     ++cnt;
     double *A, *B, *C;
@@ -20,15 +24,24 @@ void have_fun()
     cudaMalloc((void **)&B, M * N * sizeof(double));
     cudaMalloc((void **)&C, M * M * sizeof(double));
 
+
+    cudaMemcpy(A, host_A, M * N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(B, host_B, M * N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(host_C, C, M * M * sizeof(double), cudaMemcpyDeviceToHost);
+    
+
     have_fun();
+    --cnt;
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
 }
-
-
 
 int main()
 {
     cnt = 0;
-    have_fun();
+    while(1)
+        have_fun();
 
     return 0;
 }
